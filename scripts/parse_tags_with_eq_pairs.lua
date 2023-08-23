8c8c2a092a46eb1678e7ef4b2254b369bea06ef1
Amy Farrell <amy.farrell@gsa.gov>
2023-08-22 17:30:27 -0700

    WIP on parsing cf log data

---
+++ scripts/parse_tags_with_eq_pairs.lua
@@ -0,0 +1,23 @@
+-- This parser is just a proof-of-concept. 
+
+KEYS_TO_PARSE = {"tags", "gauge"}
+
+-- Splits a string of foo="bar" pairs and makes parsable json out of it. 
+function parse_tags_with_eq_pairs(tag, timestamp, record)
+    for k, v in pairs(KEYS_TO_PARSE) do
+
+        if (record[v] ~= nil) then 
+            record[v] = eq_pairs_to_json_string(record[v])
+	end
+    end
+     -- 2 leaves timestamp unchanged
+    return 2, timestamp, record
+end
+
+function eq_pairs_to_json_string(orig_string)
+    local new_string = string.gsub(orig_string, "(%S+)=(%S+)", "\"%1\":%2,")
+    -- trim off that last , and add curly braces around: 
+    new_string = string.gsub(new_string, "(.+),$", "{%1}")
+    return new_string
+end    
+
